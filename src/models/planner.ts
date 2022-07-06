30548d328dadfdd3ea4b1e54b80765da6f78ec46
bwright <bwright@roku.com>
2022-07-06 16:32:04 +0100

    a bit more table

--- src/models/planner.ts
+++ src/models/planner.ts
@@ -1,15 +1,18 @@
 import * as find from '../utils/find';
 import * as graphics from '../utils/graphics';
 import PlannerDate from '../utils/planner-date';
-import './svg.css';
+import './planner.css';
 
 export default class Planner {
-    private plannerSVG: HTMLElement;
+    private planner: HTMLElement;
+    private headerTemplate: HTMLTemplateElement;
+
     private days: PlannerDate[];
     private scale: number;
 
     constructor(doc: Document, days: PlannerDate[], scale: number) {
-        this.plannerSVG = find.byId(doc, "planner");
+        this.planner = find.byId(doc, "planner");
+        this.headerTemplate = find.templateById(doc, "header-tpl");
         this.days = days;
         this.scale = scale;
     }
@@ -18,77 +21,27 @@ export default class Planner {
         await graphics.nextFrame();
 
         this.clear();
-
-        if (!this.plannerSVG.parentElement) {
-            throw new Error("drawing space can't see its parent");
-        }
-
-        const width = this.scale * this.days.length;
-        const height = this.scale * 3;
-
-        this.plannerSVG.setAttribute("viewBox", `0 0 ${width} ${height}`);
-        this.plannerSVG.setAttribute("width", "100%");
-        this.plannerSVG.setAttribute("height", "auto");
-        this.plannerSVG.append(...this.grid(width, height, this.days.length));
-        this.plannerSVG.append(...this.headers(width, this.days));
+        this.planner.append(...this.headers(this.days));
     }
 
     clear() {
-        while (this.plannerSVG.firstChild) {
-            this.plannerSVG.removeChild(this.plannerSVG.firstChild);
+        while (this.planner.firstChild) {
+            this.planner.removeChild(this.planner.firstChild);
         }
     }
 
-    private grid(width: number, height: number, days: number): SVGLineElement[] {
-        const lines = [];
+    private headers(days: PlannerDate[]): Node[] {
+        return days.map((day: PlannerDate, index: number) => {
+            if (!this.headerTemplate.content.firstElementChild) {
+                throw new Error("header template did not contain a valid HTML element");
+            }
 
-        lines.push(makeLine(0, 0, width, 0), makeLine(0, height, width, height))
+            const header = this.headerTemplate.content.firstElementChild.cloneNode(true);
 
-        for (let x = 0; x <= days; x++) {
-            const ratio = x / days;
-            lines.push(makeLine(width * ratio, 0, width * ratio, height))
-        }
-
-        return lines;
+            const para = header as HTMLParagraphElement;
+            para.innerText = day.toShortDay();
+            para.title = day.toString();
+            return para;
+        });
     }
-
-    private headers(width: number, days: PlannerDate[]): SVGTextElement[] {
-        return days.map((day: PlannerDate, index: number) =>
-            makeText(width * (index + 0.5) / days.length, 24, day.toShortDay())
-        );
-    }
-}
-
-function makeLine(x1: number, y1: number, x2: number, y2: number): SVGLineElement {
-    const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-
-    line.setAttribute("x1", x1.toFixed(0));
-    line.setAttribute("y1", y1.toFixed(0));
-    line.setAttribute("x2", x2.toFixed(0));
-    line.setAttribute("y2", y2.toFixed(0));
-    line.setAttribute("stroke-width", "4");
-
-    return line;
-}
-
-function makeText(x: number, y: number, content: string): SVGTextElement {
-    const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
-
-    text.innerHTML = content;
-
-    text.setAttribute("x", (x - (getTextWidth(content, '16px serif') / 2)).toFixed(0));
-    text.setAttribute("y", y.toFixed(0));
-
-    return text;
-}
-
-const getTextWidth = (text: string, font: string): number => {
-    const element = document.createElement('canvas');
-    const context = element.getContext('2d');
-    if (context === null) {
-        throw new Error("cannot measure text, failed to get context");
-    }
-
-    context.font = font;
-    return context.measureText(text).width;
 }
